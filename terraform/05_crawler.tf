87f4a7053e4d9392f6a8c97caf1a679115f363c2
usmanZeki <usman@thezeki.com>
2024-06-20 17:47:59 +0500

    pregrant crawler add

--- terraform/05_crawler.tf
+++ terraform/05_crawler.tf
@@ -1,6 +1,6 @@
 resource "aws_glue_crawler" "patents_uspto" {
   database_name = aws_glue_catalog_database.zeki_patents.name
-  name          = "patents_uspto"
+  name          = "patents_granted_uspto"
   role          = local.arn
   description   = "AWS Glue Crawler for crawling over the patents data"
   table_prefix  = "bronze_granted_"
@@ -20,6 +20,40 @@ resource "aws_glue_crawler" "patents_uspto" {
 
   s3_target {
     path = "s3://${local.s3_bucket_name}/loading/granted/parquet/"
+  }
+
+  schema_change_policy {
+    delete_behavior = "DELETE_FROM_DATABASE"
+    update_behavior = "UPDATE_IN_DATABASE"
+  }
+
+  recrawl_policy {
+    recrawl_behavior = "CRAWL_EVERYTHING"
+  }
+}
+
+resource "aws_glue_crawler" "patents_pre_granted_uspto" {
+  database_name = aws_glue_catalog_database.zeki_patents.name
+  name          = "patents_pre_granted_uspto"
+  role          = local.arn
+  description   = "AWS Glue Crawler for crawling over the patents data"
+  table_prefix  = "bronze_pre_grant_"
+
+  configuration = jsonencode(
+    {
+      Grouping = {
+        TableGroupingPolicy = "CombineCompatibleSchemas"
+        TableLevelConfiguration = 5
+      }
+      CrawlerOutput = {
+        Tables = { TableThreshold = 3 }
+      }
+      Version = 1
+    }
+  )
+
+  s3_target {
+    path = "s3://${local.s3_bucket_name}/loading/pre_grant/parquet/"
   }
 
   schema_change_policy {
